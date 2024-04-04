69d033988f91838c13fa139fa78b2d45e1f39c4b
zelermehdi <gamouh.mehdi@gmail.com>
2024-04-04 16:12:58 +0200

    ajout authentification (login/register)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,11 +20,11 @@
                 </div>
             </div>
         </div>
-        
+
         <div class="col-md-4">
             <div class="info-box">
                 <span class="info-box-icon bg-success"><i class="fas fa-users"></i></span>
-        
+
                 <div class="info-box-content">
                     <span class="info-box-text">Utilisateurs</span>
                     <span class="info-box-number">{{ $usersCount }}</span>
@@ -35,61 +35,61 @@
         <div class="col-md-4">
             <div class="info-box">
                 <span class="info-box-icon bg-primary"><i class="fas fa-solar-panel"></i></span>
-        
+
                 <div class="info-box-content">
                     <span class="info-box-text">Installations</span>
                     <span class="info-box-number">{{ $installationsCount }}</span>
                 </div>
             </div>
         </div>
-        
+
         <div class="col-md-4">
             <div class="info-box">
                 <span class="info-box-icon bg-warning"><i class="fas fa-exclamation-triangle"></i></span>
-        
+
                 <div class="info-box-content">
                     <span class="info-box-text">Alertes Actives</span>
                     <span class="info-box-number">{{ $activeAlertsCount }}</span>
                 </div>
             </div>
         </div>
-        
+
         <div class="col-md-4">
             <div class="info-box">
                 <span class="info-box-icon bg-info"><i class="fas fa-wrench"></i></span>
-        
+
                 <div class="info-box-content">
                     <span class="info-box-text">Maintenance en cours</span>
                     <span class="info-box-number">{{ $maintenanceCount }}</span>
                 </div>
             </div>
         </div>
-        
+
         <div class="col-md-4" id="energyProductionChartLink">
             <div class="info-box">
                 <span class="info-box-icon bg-success"><i class="fas fa-bolt"></i></span>
-        
+
                 <div class="info-box-content">
                     <span class="info-box-text">Énergie totale générée</span>
                     <span class="info-box-number">{{ $totalEnergyGenerated }} kWh</span>
                 </div>
             </div>
         </div>
-        
+
         <script>
             document.getElementById('energyProductionChartLink').addEventListener('click', function() {
                 window.location.href = "{{ route('dashboard.energyProductionChart') }}";
             });
         </script>
-        
-        
 
 
 
-        
-            </div>
 
-            
+
+
+    </div>
+
+
 
     <!-- Plus de contenu ici -->
 @stop
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
 
 /*
@@ -17,5 +18,15 @@ use App\Http\Controllers\DashboardController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('/dashboard/energy-production-chart', [DashboardController::class, 'energyProductionChart'])->name('dashboard.energyProductionChart');
+require __DIR__.'/auth.php';
